ffbc6c0ba89b41f0acc22211d2941930e7b0bbe7
Manuel Krebber <manuel.krebber@granikos.eu>
2016-01-30 20:23:04 +0100

    Stuff

--- lib.js
+++ lib.js
@@ -11,9 +11,9 @@ var Engine = Matter.Engine,
   MouseConstraint = Matter.MouseConstraint,
   Constraint = Matter.Constraint,
   Events = Matter.Events,
-  Body = Matter.Body;
+  Body = Matter.Body
 
-var noncolliding = Matter.Body.nextGroup(true);
+var noncolliding = Matter.Body.nextGroup(true)
 var chainStyle = {
   lineWidth: 3,
   strokeStyle: '#F00'
@@ -27,9 +27,9 @@ var chainStyle = {
     length: 20 // optional
   }
 */
-function attachWithRope(world, opts) { //from, body, bodyPoint, length) {
+function attachWithRope (world, opts) { // from, body, bodyPoint, length) {
   var body = opts.to
-  var bodyPoint = opts.toPoint || {x:0, y:0}
+  var bodyPoint = opts.toPoint || {x: 0, y: 0}
   var length = opts.length || Math.abs(Matter.Vector.magnitude(Matter.Vector.sub(body.position, opts.fromPoint)))
   function removeRope () {
     var self = this
@@ -38,39 +38,39 @@ function attachWithRope(world, opts) { //from, body, bodyPoint, length) {
       Matter.Composite.removeConstraintAt(ropeB, i)
     }, 0)
   }
-  var ropeB = Matter.Composites.stack(opts.fromPoint.x, opts.fromPoint.y, 1, Math.ceil(length / 40), 20, 25, function(x, y) {
+  var ropeB = Matter.Composites.stack(opts.fromPoint.x, opts.fromPoint.y, 1, Math.ceil(length / 40), 20, 25, function (x, y) {
     var c = Matter.Bodies.circle(x, y, 5, {
       label: 'rope',
       collisionFilter: { group: noncolliding },
       render: {
         visible: false
       }
-    });
+    })
     c.label = 'rope'
     c.removeRope = removeRope
     return c
-  });
+  })
 
   Matter.Composites.chain(ropeB, 0, 0, 0, 0, {
     stiffness: 0.7,
     length: 40,
     render: chainStyle
-  });
+  })
 
-  Matter.Composite.add(ropeB, Matter.Constraint.create({ 
+  Matter.Composite.add(ropeB, Matter.Constraint.create({
     bodyB: ropeB.bodies[0],
     pointA: opts.fromPoint,
     stiffness: 0.7,
     render: chainStyle
-  }));
-  Matter.Composite.add(ropeB, Matter.Constraint.create({ 
+  }))
+  Matter.Composite.add(ropeB, Matter.Constraint.create({
     bodyA: ropeB.bodies[ropeB.bodies.length - 1],
     bodyB: body,
     pointB: bodyPoint,
     stiffness: 0.7,
     length: 10,
     render: chainStyle
-  }));
+  }))
   World.add(world, [ropeB])
 }
 
@@ -97,8 +97,8 @@ function addSword (engine, pos) {
   function swordContraint (sword) {
     var moveswordto = pos
     var sworddirection = 0
-    var mouseConstraint = MouseConstraint.create(engine);
-    Matter.Events.on(mouseConstraint, 'mousedown', function (event){
+    var mouseConstraint = MouseConstraint.create(engine)
+    Matter.Events.on(mouseConstraint, 'mousedown', function (event) {
       moveswordto = Vector.clone(event.mouse.position)
     })
     Matter.Events.on(mouseConstraint, 'mousemove', function (event) {
@@ -137,134 +137,136 @@ function addSword (engine, pos) {
 }
 
 function waitForSword (engine) {
-  var mouseConstraint = MouseConstraint.create(engine);
-  function listener (event){
+  var mouseConstraint = MouseConstraint.create(engine)
+  function listener (event) {
     Matter.Events.off(mouseConstraint, 'mouseup', listener)
     addSword(engine, Vector.clone(event.mouse.position))
   }
   Matter.Events.on(mouseConstraint, 'mouseup', listener)
 }
 
-var boneWidth = 116;
-var boneHeadWidth = 209;
-var boneHeadHeight = 146;
-var boneGroup = Body.nextGroup(true);
+var boneWidth = 116
+var boneHeadWidth = 209
+var boneHeadHeight = 146
+var boneGroup = Body.nextGroup(true)
 
 var _boneHeadCache = {}
 
-function createBone(x1, y1, x2, y2, w) {
-    var vertices;
-    
-        var h = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
-        var cx = (x1 + x2) / 2;
-        var cy = (y1 + y2) / 2;
-        var scale = w / boneWidth;
-        
-        if (_boneHeadCache[w]) {
-            vertices = _boneHeadCache[w];
-        } else {        
-            var pathWidth = boneEndBounds.max.x - boneEndBounds.min.x;
-            var pathHeight = boneEndBounds.max.y - boneEndBounds.min.y;
-            var headScaleX = scale * boneHeadWidth / pathWidth*0.95;
-            var headScaleY = scale * boneHeadHeight / pathHeight*0.95;
-        
-            vertices = Vertices.scale(Vertices.create(boneEndPath), headScaleX, headScaleY);            
-        }
-                    
-                    var end1 = Bodies.fromVertices(cx, cy - h/2, [vertices], {
-                        render: {
-                            fillStyle: 'none',
-                            strokeStyle: '#FF0000',
-                            sprite: {
-                                texture: 'img/bone_end.png',
-                                xScale: scale,
-                                yScale: scale,
-                            }
-                        },
-                        collisionFilter: { group: boneGroup },
-                        isStatic: true
-                    }, true);
-                    var end2 = Bodies.fromVertices(cx, cy + h/2, [vertices], {
-                        render: {
-                            fillStyle: 'none',
-                            strokeStyle: '#FF0000',
-                            sprite: {
-                                texture: 'img/bone_end.png',
-                                xScale: scale,
-                                yScale: scale,
-                            }
-                        },
-                        angle: Math.PI,
-                        collisionFilter: { group: boneGroup },
-                        isStatic: true
-                    }, true);
-                    
-                    var shaft = Bodies.rectangle(cx, cy, w, h, { 
-                        render: {
-                            sprite: {
-                                texture: 'img/bone.png',
-                                xScale: scale,
-                                yScale: h
-                            }
-                        },
-                        collisionFilter: { group: boneGroup },
-                        isStatic: true
-                    });
-                    
-                    var angle = -Math.atan((x1-x2)/(y1-y2));
-                    
-                    var bone = Composite.create({bodies: [shaft, end1, end2] });
-                    Composite.rotate(bone, angle, {x: cx, y: cy});
-                    
-                    World.add(engine.world, bone);
-                    
-                    return bone;
+function createBone (x1, y1, x2, y2, w) {
+  var vertices
+
+  var h = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+  var cx = (x1 + x2) / 2
+  var cy = (y1 + y2) / 2
+  var scale = w / boneWidth
+
+  if (_boneHeadCache[w]) {
+    vertices = _boneHeadCache[w]
+  } else {
+    var pathWidth = boneEndBounds.max.x - boneEndBounds.min.x
+    var pathHeight = boneEndBounds.max.y - boneEndBounds.min.y
+    var headScaleX = scale * boneHeadWidth / pathWidth * 0.95
+    var headScaleY = scale * boneHeadHeight / pathHeight * 0.95
+
+    vertices = Vertices.scale(Vertices.create(boneEndPath), headScaleX, headScaleY)
+  }
+
+  var end1 = Bodies.fromVertices(cx, cy - h / 2, [vertices], {
+    render: {
+      fillStyle: 'none',
+      strokeStyle: '#FF0000',
+      sprite: {
+        texture: 'img/bone_end.png',
+        xScale: scale,
+        yScale: scale,
+      }
+    },
+    collisionFilter: { group: boneGroup },
+    isStatic: true
+  }, true)
+  var end2 = Bodies.fromVertices(cx, cy + h / 2, [vertices], {
+    render: {
+      fillStyle: 'none',
+      strokeStyle: '#FF0000',
+      sprite: {
+        texture: 'img/bone_end.png',
+        xScale: scale,
+        yScale: scale,
+      }
+    },
+    angle: Math.PI,
+    collisionFilter: { group: boneGroup },
+    isStatic: true
+  }, true)
+
+  var shaft = Bodies.rectangle(cx, cy, w, h, {
+    render: {
+      sprite: {
+        texture: 'img/bone.png',
+        xScale: scale,
+        yScale: h
+      }
+    },
+    collisionFilter: { group: boneGroup },
+    isStatic: true
+  })
+
+  var angle = -Math.atan((x1 - x2) / (y1 - y2))
+
+  var bone = Composite.create({bodies: [shaft, end1, end2] })
+  Composite.rotate(bone, angle, {x: cx, y: cy})
+
+  World.add(engine.world, bone)
+
+  return bone
 }
 
 var organs = {
-    liver: {
-        image: 'img/liver.png',
-        collision: 'img/liver.svg',
-    },
-    heart: {
-        image: 'img/heart.png',
-        collision: 'img/heart.svg',
-    },
-    kidney: {
-        image: 'img/kidney.png',
-        collision: 'img/kidney.svg',
-    },
-    lungs: {
-        image: 'img/lungs.png',
-        collision: 'img/lungs.svg',
-    },
-    stomach: {
-        image: 'img/stomach.png',
-        collision: 'img/stomach.svg',
-    },
-};
+  liver: {
+    image: 'img/liver.png',
+    collision: 'img/liver.svg',
+  },
+  heart: {
+    image: 'img/heart.png',
+    collision: 'img/heart.svg',
+  },
+  kidney: {
+    image: 'img/kidney.png',
+    collision: 'img/kidney.svg',
+  },
+  lungs: {
+    image: 'img/lungs.png',
+    collision: 'img/lungs.svg',
+  },
+  stomach: {
+    image: 'img/stomach.png',
+    collision: 'img/stomach.svg',
+  },
+}
 
-function createOrgan(organ, x, y, scale) {
-    scale = scale || 1;
-    organ = organs[organ];
-                    var bds = organ._bounds;
-                    var pathWidth = bds.max.x - bds.min.x;
-                    var pathHeight = bds.max.y - bds.min.y;
-                    var vertices = Vertices.scale(organ._path, organ._width * scale / pathWidth, organ._height * scale / pathHeight);
-                    
-                    var o = Bodies.fromVertices(x, y, [vertices], {
-                        render: {
-                            fillStyle: 'none',
-                            strokeStyle: '#FF0000',
-                            sprite: {
-                                texture: organ.image,
-                                xScale: scale,
-                                yScale: scale
-                            }
-                        }
-                    }, true);
+function createOrgan (organ, x, y, scale) {
+  scale = scale || 1
+  organ = organs[organ]
+  var bds = organ._bounds
+  var pathWidth = bds.max.x - bds.min.x
+  var pathHeight = bds.max.y - bds.min.y
+  var vertices = Vertices.scale(organ._path, organ._width * scale / pathWidth, organ._height * scale / pathHeight)
 
-                    World.add(engine.world, [o]);
-                    
-                    return o;
-                }
+  var o = Bodies.fromVertices(x, y, [vertices], {
+    render: {
+      fillStyle: 'none',
+      strokeStyle: '#FF0000',
+      sprite: {
+        texture: organ.image,
+        xScale: scale,
+        yScale: scale
+      }
+    }
+  }, true)
+
+  World.add(engine.world, [o])
+
+  level.organs.push(o)
+
+  return o
+}
--- main.js
+++ main.js
@@ -1,85 +1,151 @@
 // Matter.js module aliases
 var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Common = Matter.Common,
-    Vertices = Matter.Vertices,
-    Vector = Matter.Vector,
-    Svg = Matter.Svg,
-    Bounds = Matter.Bounds,
-    MouseConstraint = Matter.MouseConstraint,
-    Constraint = Matter.Constraint,
-    Events = Matter.Events,
-    Body = Matter.Body;
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Common = Matter.Common,
+  Vertices = Matter.Vertices,
+  Vector = Matter.Vector,
+  Svg = Matter.Svg,
+  Bounds = Matter.Bounds,
+  MouseConstraint = Matter.MouseConstraint,
+  Constraint = Matter.Constraint,
+  Events = Matter.Events,
+  Body = Matter.Body
 
 // create a Matter.js engine
-var engine = Engine.create(document.body, {render: RenderPixi2.create({element: document.body, options: {debug: false}}) });
-            
-            var queue = new createjs.LoadQueue(true);            
-            
-var loadingText = new PIXI.Text('Loading...',{font : '24px Arial', fill : 0xff1010, align : 'center'});
-engine.render.textContainer.addChild(loadingText);
-            
-            function loadFiles() {
-                for (var organ in organs)
-                {
-                    queue.loadFile(organs[organ].collision, false); 
-                    queue.loadFile(organs[organ].image, false);               
-                }
-                
-                queue.loadFile('img/bone_end_collision.svg', false); 
-                queue.loadFile('img/bone_end.png', false);  
-                queue.loadFile('img/bone.png', false);  
-                
-                 queue.on("complete", function() {
-                     engine.render.textContainer.removeChild(loadingText);
-                     buildPaths();
-                     createLevel();
-                 });
-                 
-                 queue.load();
-            }
-            
-            var boneEndPath, boneEndBounds;
-            
-            function buildPath(file) {
-                var data = queue.getResult(file);
-                var path = $(data).find('path')[0];
-                return Svg.pathToVertices(path, 10);                
-            }
-            
-            function buildPaths() {
-                boneEndPath = buildPath('img/bone_end_collision.svg');
-                boneEndBounds = Bounds.create(boneEndPath);
-                
-                for (var organ in organs)
-                {
-                    organ = organs[organ];
-                    var p = organ._path = buildPath(organ.collision, false);  
-                    organ._bounds = Bounds.create(p);
-                    var img = queue.getResult(organ.image);
-                    organ._width = img.width;     
-                    organ._height = img.height;       
-                }              
-            }
-            
-            function createLevel() {                
-                createBone(30, 30, 780, 30, 40);
-                createBone(30, 85, 30, 515, 40);
-                createBone(50, 570, 750, 570, 40);
-                createBone(770, 85, 770, 515, 40);
-                
-                createOrgan('lungs', 400, 400, 0.2);
-                
-                World.add(engine.world, [MouseConstraint.create(engine)]);
-            }
-            
-            loadFiles();
+var engine = Engine.create(document.body, { render: RenderPixi2.create({element: document.body, options: {debug: false}}) })
 
-        var renderOptions = engine.render.options;
-        renderOptions.showAngleIndicator = false;
-        renderOptions.wireframes = false;
-        renderOptions.showConvexHull = true;
+var queue = new createjs.LoadQueue(true)
+
+var loadingText = new PIXI.Text('Loading...', {font: '24px Arial', fill: 0xff1010, align: 'center'})
+engine.render.textContainer.addChild(loadingText)
+
+function loadFiles () {
+  for (var organ in organs) {
+    queue.loadFile(organs[organ].collision, false)
+    queue.loadFile(organs[organ].image, false)
+  }
+
+  queue.loadFile('img/bone_end_collision.svg', false)
+  queue.loadFile('img/bone_end.png', false)
+  queue.loadFile('img/bone.png', false)
+
+  queue.on('complete', function () {
+    engine.render.textContainer.removeChild(loadingText)
+    buildPaths()
+    createLevel()
+  })
+
+  queue.load()
+}
+
+var boneEndPath, boneEndBounds
+
+function buildPath (file) {
+  var data = queue.getResult(file)
+  var path = $(data).find('path')[0]
+  return Svg.pathToVertices(path, 10)
+}
+
+function buildPaths () {
+  boneEndPath = buildPath('img/bone_end_collision.svg')
+  boneEndBounds = Bounds.create(boneEndPath)
+
+  for (var organ in organs) {
+    organ = organs[organ]
+    var p = organ._path = buildPath(organ.collision, false)
+    organ._bounds = Bounds.create(p)
+    var img = queue.getResult(organ.image)
+    organ._width = img.width
+    organ._height = img.height
+  }
+}
+
+var level = {
+  organs: [],
+  targetZones: [{
+    min: {
+      x: 50,
+      y: 500
+    },
+    max: {
+      x: 200,
+      y: 560
+    }
+  }, {
+    min: {
+      x: 500,
+      y: 60
+    },
+    max: {
+      x: 700,
+      y: 150
+    }
+  }],
+  score: 0
+}
+
+function createLevel () {
+  createBone(30, 30, 780, 30, 40)
+  createBone(30, 85, 30, 515, 40)
+  createBone(50, 570, 750, 570, 40)
+  createBone(770, 85, 770, 515, 40)
+
+  createOrgan('lungs', 200, 400, 0.2)
+
+  createOrgan('liver', 400, 400, 0.2)
+
+  createOrgan('heart', 600, 400, 0.2)
+
+  Events.on(engine, 'tick', function (event) {
+    for (var i = 0; i < level.targetZones.length; i++) {
+      var zone = level.targetZones[i]
+      var result = Matter.Query.region(level.organs, zone)
+      for (var j = 0; j < result.length; j++) {
+        var center = Vertices.centre(result[j].vertices)
+        if (Bounds.contains(zone, center)) {
+          World.remove(engine.world, result[j])
+          level.organs.splice(level.organs.indexOf(result[j]), 1)
+          level.score++
+          refreshScore()
+          break
+        }
+      }
+    }
+  })
+
+  World.add(engine.world, [MouseConstraint.create(engine)])
+
+  for (var i = 0; i < level.targetZones.length; i++) {
+    var zone = level.targetZones[i]
+    var graphics = new PIXI.Graphics()
+
+    graphics.beginFill(0x0088FF, 0.2)
+    graphics.drawRect(zone.min.x, zone.min.y, zone.max.x - zone.min.x, zone.max.y - zone.min.y)
+
+    engine.render.textContainer.addChild(graphics)
+  }
+  
+  refreshScore()
+}
+
+var scoreText
+function refreshScore () {
+  if (!scoreText) {
+    scoreText = new PIXI.Text('Score: 0', {font: '24px Arial', fill: 0xff1010, align: 'center'})
+    engine.render.textContainer.addChild(scoreText)
+  }
+
+  scoreText.setText('Score: ' + level.score)
+}
+
+loadFiles()
+
+var renderOptions = engine.render.options
+renderOptions.showAngleIndicator = false
+renderOptions.wireframes = false
+renderOptions.showConvexHull = true
+engine.enableSleeping = true
 
 // run the engine
-Engine.run(engine);
+Engine.run(engine)
