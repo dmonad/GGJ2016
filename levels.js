f45ae0c78a6b2409b57a5082bde50ecb9e6bdaac
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2016-01-31 15:04:34 +0100

    add license

--- levels.js
+++ levels.js
@@ -39,63 +39,17 @@ window.levels = [
     putFireChakra(engine, {
       x: 200,
       y: 370
-    })
+    }, 2)
 
     putFireChakra(engine, {
       x: 600,
       y: 450
-    })
+    }, 2)
     waitForSword(engine, level)
 
     return level
   },
   function level2 (engine) {
-    var level = {
-      attempts: 0,
-      maxAttempts: 2,
-      organs: [],
-      targetZones: [{
-        min: {
-          x: 700,
-          y: 20
-        },
-        max: {
-          x: 970,
-          y: 200
-        }
-      }],
-      score: 0
-    }
-    
-    createBone (670, 200, 680, 330, 30)
-
-    var heart = createOrgan('heart', 370, 400, 0.2, level)
-
-    attachWithRope(engine.world, {
-      fromPoint: {x: 600, y: 170},
-      to: heart
-    })
-
-    attachWithRope(engine.world, {
-      fromPoint: {x: 120, y: 170},
-      to: heart
-    })
-
-    putFireChakra(engine, {
-      x: 600,
-      y: 450
-    }, 10)
-    
-    putFireChakra(engine, {
-      x: 170,
-      y:460
-    }, 5)
-
-    waitForSword(engine, level)
-
-    return level
-  },
-  function level3 () {
     var level = {
       attempts: 0,
       maxAttempts: 2,
@@ -141,7 +95,7 @@ window.levels = [
 
     return level
   },
-  function level4 (engine) {
+  function level3 (engine) {
     var level = {
       attempts: 0,
       maxAttempts: 2,
@@ -179,6 +133,45 @@ window.levels = [
       x: 600,
       y: 450
     }, bone)
+
+    waitForSword(engine, level)
+
+    return level
+  },
+  function level4 (engine) {
+    var level = {
+      attempts: 0,
+      maxAttempts: 2,
+      organs: [],
+      targetZones: [{
+        min: {
+          x: 50,
+          y: 670
+        },
+        max: {
+          x: 330,
+          y: 750
+        }
+      }],
+      score: 0
+    }
+    
+    var bone = createBone (250, 400, 570, 750, 30)
+
+    var heart = createOrgan('heart', 370, 420, 0.2, level)
+
+    attachWithRope(engine.world, {
+      fromPoint: {x: 750, y: 170},
+      to: heart
+    })
+    attachWithRope(engine.world, {
+      fromPoint: {x: 270, y: 170},
+      to: heart
+    })
+    putWaterChakra(engine, {
+      x: 600,
+      y: 450
+    }, 2000)
 
     waitForSword(engine, level)
 
